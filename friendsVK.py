9cc6143d9351feecc8d0c7022ea337da25b478c2
Alex Khagush <baldr11@mail.ru>
2020-07-13 15:05:12 +0300

    fix variation

--- friendsVK.py
+++ friendsVK.py
@@ -2,19 +2,29 @@ import requests
 import time
 import json
 
-TOKEN = input("Введите токен ") # "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
-USER_id = input("Введите id пользователя ") # "171691064" 18526683
 
+TOKEN = input("Введите токен ") 
+USER_id = input("Введите пользователя ") 
 
+start_time = time.time()
 class USER:
 
-    def __init__(self, TOKEN):
-        self.token = TOKEN
-        return
+
+    def __init__(self):
+        pass
+
+    def name_user(self):
+        if USER_id.isdigit():
+            self.USER_id = USER_id
+            return self.USER_id 
+        else:
+            response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.120&code=return API.utils.resolveScreenName({"screen_name":"'+USER_id+'"});')
+            self.USER_id = response.json()['response']['object_id']
+            return self.USER_id
 
     def get_groups(self):
-        groupset = set()       
-        response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.107&code=return API.groups.get({"user_id":"'+USER_id+'","count":"1000"});')
+        groupset = set()
+        response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.107&code=return API.groups.get({"user_id":"'+str(self.name_user())+'","count":"1000"});')
         data = response.json()['response']['items']
         for i in data:
             groupset.add(i)
@@ -22,7 +32,7 @@ class USER:
 
     def get_friends(self):
         friendset = set()
-        response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.107&code=return API.friends.get({"user_id":"'+USER_id+'","count":"1000","extended": 0, "order":"name"});')
+        response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.107&code=return API.friends.get({"user_id":"'+str(self.name_user())+'","count":"1000","extended": 0, "order":"name"});')
         data = response.json()['response']['items']
         for i in data:
             friendset.add(i)
@@ -30,10 +40,11 @@ class USER:
 
     def get_friends_groups(self):
         groups_friend_set = set()
-        groups = self.get_groups()        
+        groups = self.get_groups()
         for i in self.get_friends():
             time.sleep(0.5)
             response = requests.post('https://api.vk.com/method/execute?access_token='+TOKEN+'&v=5.107&code=return API.groups.get({"user_id":"'+str(i)+'","count":"1000","extended": 0});')
+            print(f"--запрос--")
             data_fr = response.json()
             if 'response' in data_fr.keys():
                 data = response.json()['response']['items']
@@ -56,6 +67,10 @@ class USER:
 
         return gotten_groups
 
-Evg = USER(TOKEN)
+
+Evg = USER()
 
 print(Evg.get_json_file())
+
+end_time = time.time()
+print(end_time - start_time)
